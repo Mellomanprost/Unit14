7bd732f3097d84abae8168e5e243e20a875f2783
Mellomanprost <anoppa@mail.ru>
2022-11-19 22:53:56 +0300

    Добавьте файлы проекта.

---
+++ Unit14(8)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unit14_8_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objects = new List<object>()
+            {
+                1,
+                "Сергей ",
+                "Андрей ",
+                300,
+            };
+
+            var names = from obj in objects where obj is string orderby obj select obj;
+            foreach (var name in names)
+                Console.WriteLine(name);
+
+            Console.WriteLine();
+           
+            foreach (var name2 in names.Where(ob => ob is string).OrderBy(ob => ob))
+                Console.WriteLine(name2);
+        }
+    }
+}
---
+++ Unit14/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unit14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+            /// without LINQ
+            //// список, куда будем сохранять выборку
+            //var orderedList = new List<string>();
+
+            //// пробежимся по массиву и добавим искомое в наш список
+            //foreach (string person in people)
+            //{
+            //    if (person.ToUpper().StartsWith("А"))
+            //        orderedList.Add(person);
+            //}
+
+            //// отсортируем список
+            //orderedList.Sort();
+            //foreach (string s in orderedList)
+            //    Console.WriteLine(s);
+
+            /// with LINQ
+            //var selectedPeople = from p in people // промежуточная переменная p 
+            //    where p.StartsWith("А") // фильтрация по условию
+            //    orderby p // сортировка по возрастанию (дефолтная)
+            //    select p; // выбираем объект и сохраняем в выборку
+
+            //foreach (string s in selectedPeople)
+                //Console.WriteLine(s);
+            /// with LINQ short
+            var selectedPeople = people.Where(p => p.StartsWith("А")).OrderBy(p => p);
+
+            foreach (string s in selectedPeople)
+                Console.WriteLine(s);
+        }
+    }
+}
