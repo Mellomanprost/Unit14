d5067467dbc4ce8e94900acb850627a3269952fe
Mellomanprost <anoppa@mail.ru>
2022-11-20 16:07:15 +0300

    Added exercise 14.1.6

---
+++ Unit14.1.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unit14._1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numsList = new List<int[]>()
+            {
+                new[] {2, 3, 7, 1},
+                new[] {45, 17, 88, 0},
+                new[] {23, 32, 44, -6},
+            };
+            //var sortNumList = from nums in numsList
+            //    from num in nums
+            //    orderby num
+            //    select num;
+            var sortNumList = numsList.SelectMany(n => n).OrderBy(s => s);
+            foreach (var item in sortNumList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
