bd1323a1755900c30590667c9e6745aa05d88c15
Mellomanprost <anoppa@mail.ru>
2022-11-21 18:24:43 +0300

    Added exercise 14.2.10

--- Unit14(8)/Program.cs
+++ Unit14(8)/Program.cs
@@ -8,22 +8,68 @@ namespace Unit14_8_
     {
         static void Main(string[] args)
         {
-            var objects = new List<object>()
+            #region Region From Start Unit
+
+            //var objects = new List<object>()
+            //{
+            //    1,
+            //    "Сергей ",
+            //    "Андрей ",
+            //    300,
+            //};
+
+            //var names = from obj in objects where obj is string orderby obj select obj;
+            //foreach (var name in names)
+            //    Console.WriteLine(name);
+
+            //Console.WriteLine();
+
+            //foreach (var name2 in objects.Where(ob => ob is string).OrderBy(ob => ob))
+            //    Console.WriteLine(name2);
+
+            #endregion
+
+            #region Region For SikpWhile, TakeWhile
+
+            // Подготовка данных
+            var cars = new List<Car>()
             {
-                1,
-                "Сергей ",
-                "Андрей ",
-                300,
+                new Car("Suzuki", "JP"),
+                new Car("Toyota", "JP"),
+                new Car("Opel", "DE"),
+                new Car("Kamaz", "RUS"),
+                new Car("Lada", "RUS"),
+                new Car("Lada", "RUS"),
+                new Car("Honda", "JP"),
             };
 
-            var names = from obj in objects where obj is string orderby obj select obj;
-            foreach (var name in names)
-                Console.WriteLine(name);
+            Console.WriteLine("Пропустим японские машины в начале списка");
+            var notJapanCars = cars.SkipWhile(car => car.CountryCode == "JP");
+
+            foreach (var notJapanCar in notJapanCars)
+                Console.WriteLine(notJapanCar.Manufacturer);
 
             Console.WriteLine();
-           
-            foreach (var name2 in objects.Where(ob => ob is string).OrderBy(ob => ob))
-                Console.WriteLine(name2);
+
+            Console.WriteLine("Теперь выберем только японские машины из начала списка");
+            var japanCars = cars.TakeWhile(car => car.CountryCode == "JP");
+
+            foreach (var japanCar in japanCars)
+                Console.WriteLine(japanCar.Manufacturer);
+
+            #endregion
+        }
+
+        public class Car
+        {
+            public string Manufacturer { get; set; }
+            public string CountryCode { get; set; }
+
+            public Car(string manufacturer, string countryCode)
+            {
+                Manufacturer = manufacturer;
+                CountryCode = countryCode;
+            }
         }
     }
 }
---
+++ Unit14.2.10/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unit14._2._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            #region My code
+
+            //while (true)
+            //{
+            //    var keyChar = Console.ReadKey().KeyChar;
+            //    Console.Clear();
+            //    if (!Char.IsDigit(keyChar))
+            //    {
+            //        Console.WriteLine("Ошибка ввода, введите число");
+            //    }
+            //    else
+            //    {
+            //        IEnumerable<Contact> pages = null;
+            //        switch (keyChar)
+            //        {
+            //            case '1':
+            //                pages = phoneBook.Take(2);
+            //                break;
+            //            case '2':
+            //                pages = phoneBook.Skip(2).Take(2);
+            //                break;
+            //            case '3':
+            //                pages = phoneBook.Skip(4).Take(2);
+            //                break;
+            //        }
+
+            //        if (pages is null)
+            //        {
+            //            Console.WriteLine($"Ошибка ввода, страницы {keyChar} не существует");
+            //            continue;
+            //        }
+
+            //        foreach (var page in pages)
+            //        {
+            //            Console.WriteLine(page.Name + " " + page.LastName + ", " + page.PhoneNumber + ", " + page.Email);
+            //        }
+            //    }
+            //}
+
+
+            #endregion
+
+            #region Code from SkillFactory
+
+            while (true)
+            {
+                // Читаем введенный с консоли символ
+                var input = Console.ReadKey().KeyChar;
+
+                // проверяем, число ли это
+                var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+
+                // если не соответствует критериям - показываем ошибку
+                if (!parsed || pageNumber < 1 || pageNumber > 3)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Страницы не существует");
+                }
+                // если соответствует - запускаем вывод
+                else
+                {
+                    // пропускаем нужное количество элементов и берем 2 для показа на странице
+                    var pageContent = phoneBook.Skip((pageNumber - 1) * 2).Take(2);
+                    Console.WriteLine();
+
+                    // выводим результат
+                    foreach (var entry in pageContent)
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+
+                    Console.WriteLine();
+                }
+            }
+
+            #endregion
+        }
+
+        public class Contact // модель класса
+        {
+            public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+            {
+                Name = name;
+                LastName = lastName;
+                PhoneNumber = phoneNumber;
+                Email = email;
+            }
+
+            public String Name { get; }
+            public String LastName { get; }
+            public long PhoneNumber { get; }
+            public String Email { get; }
+        }
+    }
+}
