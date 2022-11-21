6e4bc1195ab987a16400b46b742c8dd9ee96d810
Mellomanprost <anoppa@mail.ru>
2022-11-21 14:18:52 +0300

    Added exercise 14.2.1

---
+++ Unit14.2.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Unit14._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха" };
+            var sortWords = words.Select(s => new
+            {
+                AnimalType = s
+            }).OrderBy(s => s.AnimalType.Length);
+            foreach (var word in sortWords)
+            {
+                Console.WriteLine(word.AnimalType);
+            }
+        }
+    }
+}
